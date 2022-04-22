7a5b6a8025f7e15a5e2e0535f715ff4a15eb993a
emmaglorypraise <emmaglorypraise@gmail.com>
2022-04-22 14:06:53 +0100

    make calculator functional

--- js/index.js
+++ js/index.js
@@ -1,10 +1,107 @@
 let calculatorValue = document.getElementById('calculator-value');
-let buttonValue = document.querySelectorAll('.number-btn').forEach(btn => btn.addEventListener('click', () => {
-    console.log(buttonValue.value)
-}));
+let numberButtons = document.querySelectorAll('.number-btn');
+let operatorButtons = document.querySelectorAll('.operator-btn');
+let equalsButton = document.querySelector('.equals-btn-color');
+let deleteButton = document.querySelector('.delete-btn');
+let resetButton = document.querySelector('.reset-btn');
 
-// buttonValue.addEventListener('click', getValue) 
+let previousValue = '';
+let currentValue = '';
+let calculator = []
 
-// function getValue() {
-//     console.log(buttonValue)
-// }
+if (currentValue < 0 || currentValue === 0 ) {
+    currentValue = 0;
+    calculatorValue.innerHTML = currentValue;
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(button.innerText)
+        let number = button.innerText;
+        appendNumber(number)
+    })
+} 
+);
+
+function appendNumber(number) {
+    if (number === '.' && currentValue.includes('.')) return
+    currentValue += number;
+    calculatorValue.innerHTML = currentValue ;
+    console.log(currentValue)
+}
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(button.innerText)
+        let operator = button.innerText;
+        console.log(previousValue)
+        console.log(currentValue)
+
+        switch(operator) {
+            case '+':
+            previousValue = currentValue;
+            calculator.push(previousValue)
+            calculatorValue.innerHTML = operator;
+            calculator.push(operator);
+            currentValue = '';
+            console.log(calculator);
+            break
+            case '-':
+            previousValue = currentValue;
+            calculator.push(previousValue)
+            calculatorValue.innerHTML = operator;
+            calculator.push(operator);
+            currentValue = '';
+            console.log(calculator);
+            break
+            case '/':
+            previousValue = currentValue;
+            calculator.push(previousValue)
+            calculatorValue.innerHTML = operator;
+            calculator.push(operator);
+            currentValue = '';
+            console.log(calculator);
+            break
+            case '*':
+            previousValue = currentValue;
+            calculator.push(previousValue)
+            calculatorValue.innerHTML = operator;
+            calculator.push(operator);
+            currentValue = '';
+            console.log(calculator);
+            break
+        }
+    })
+} 
+);
+
+equalsButton.addEventListener('click', button => {
+    console.log(calculator);
+    calculator.push(currentValue)
+    let calculate = eval(calculator.join(''));
+    console.log(calculate);
+    calculatorValue.innerHTML = calculate;
+    currentValue = '';
+    calculator = [];
+})
+
+deleteButton.addEventListener('click', button => {
+    console.log(currentValue);
+    currentValue = currentValue.slice(0, -1);
+    calculatorValue.innerHTML = currentValue;
+    if (currentValue === ''){
+        console.log('empty')
+        calculatorValue.innerHTML = 0;
+    }
+    console.log(currentValue);
+})
+
+resetButton.addEventListener('click', button => {
+    previousValue = '';
+    currentValue = '';
+    calculator = [];
+    calculatorValue.innerHTML = '0000';
+    console.log(currentValue);
+    console.log(previousValue);
+    console.log(calculator);
+})
